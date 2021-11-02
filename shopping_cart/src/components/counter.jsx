82623a7a4e1e637fd661da73ddf387e96f6c3eab
Phantonym21 <71942501+Phantonym21@users.noreply.github.com>
2021-11-02 16:08:00 +0530

    updated shopping_cart

--- shopping_cart/src/components/counter.jsx
+++ shopping_cart/src/components/counter.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from "react"; // Importing Component class from React to use its methods in Counter class //imrc
 
 class Counter extends React.Component {
-  state = {
-    count: 0,
-  };
   // Counter class is subclass of Component //cc
+  state = {
+    count: 1,
+  };
+  style = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()} </span>
-        <button>Increment</button>
+        <span style={{ fontSize: 15 }} className={this.getState()}>
+          {this.formatCount()}
+        </span>
+        <button
+          style={{ fontSize: 15, fontWeight: "bold" }}
+          className="btn-primary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
+  getState() {
+    let stats = " m-3 p-2 badge ";
+    stats += this.state.count === 0 ? "bg-warning" : "bg-primary";
+    return stats;
+  }
+
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : <button>Buuger</button>;
+    // const { count } = this.state;
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
