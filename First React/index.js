9d2a4544efec702552b0baefa331823ff708056f
Phantonym21 <71942501+Phantonym21@users.noreply.github.com>
2022-03-24 13:03:31 +0530

    added first react

---
+++ First React/index.js
@@ -0,0 +1,8 @@
+ReactDOM.render(<h1>Buuger</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul> <h2>List of Items</h2> <li>Ball</li><li>Bat</li></ul>,
+    document.getElementById("leaf1")
+ )
+// ReactDOM.render(<li>Ball</li>, document.getElementById("root"))
+// ReactDOM.render(<li>Bat</li>, document.getElementById("root"))
